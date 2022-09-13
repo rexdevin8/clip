f1a194562b289e1c91c0c9402ced315419cf67e4
D-Soft Labs <67311738+rexdevin8@users.noreply.github.com>
2022-09-13 18:11:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-import pyperclip
-s1 = "Hello world"
-pyperclip.copy(s1)
-s2 = pyperclip.paste()
-print(s2)
+# import pyperclip
+# s1 = "Hello world"
+# pyperclip.copy(s1)
+# s2 = pyperclip.paste()
+# print(s2)
+import clipboard
+clipboard.copy("hello devin")  
+text = clipboard.paste() 
